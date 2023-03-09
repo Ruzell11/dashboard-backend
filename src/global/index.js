4b3f8f15c4ff4c5e0a89b2b26bb27c2e979cc480
Ruzell03 <ruzell@matchmo.ph>
2023-03-09 11:34:27 +0800

    add in global config

--- src/global/index.js
+++ src/global/index.js
@@ -1,5 +1,19 @@
-export const HTTP_OK = 200;
-export const HTTP_BAD_REQUEST = 400;
+const HTTP_OK = 200;
+const HTTP_BAD_REQUEST = 400;
 
-export const SUCCESS = true;
-export const FAILED = false;
+const SUCCESS = true;
+const FAILED = false;
+
+const SUPER_ADMIN = 1;
+const SUB_ADMIN = 2;
+const READ_ONLY = 3;
+
+module.exports = {
+  HTTP_BAD_REQUEST,
+  HTTP_OK,
+  SUCCESS,
+  FAILED,
+  SUB_ADMIN,
+  SUPER_ADMIN,
+  READ_ONLY,
+};
