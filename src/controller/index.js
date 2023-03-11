d90a700a6f950f63741dc50772ea342b14477b18
Ruzell03 <ruzell@matchmo.ph>
2023-03-11 13:38:50 +0800

    implement it also in get Team member list

--- src/controller/index.js
+++ src/controller/index.js
@@ -122,12 +122,15 @@ const createUserController = () => {
   const GetTeamMembers = async (req, res) => {
     const created_by_id = req.params.id
 
-    const user_profile = await UserModel.findById(created_by_id);
+    let user_profile = await UserModel.findById(created_by_id);
 
     if (!user_profile) {
-      return res
-        .status(HTTP_BAD_REQUEST)
-        .json({ success: FAILED, message: "User does not exist" });
+      user_profile = await TeamMember.findById(created_by_id);
+      if (!user_profile) {
+        return res
+          .status(HTTP_BAD_REQUEST)
+          .json({ success: FAILED, message: "User does not exist" });
+      }
     }
 
     const listOfMember = await TeamMember.find({ created_by: created_by_id })
@@ -141,7 +144,7 @@ const createUserController = () => {
     return res.status(HTTP_OK).json({
       success: SUCCESS,
       message: 'List of your team members',
-      team_leader_name: user_profile.username,
+      created_by: user_profile.username,
       listOfMember
     })
   }
